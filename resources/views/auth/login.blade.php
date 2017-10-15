85b361c7baf13bc551292232947523bf9d409ea6
MD SAMI <mdsami.diu@gmail.com>
2017-10-16 05:25:44 +0600

    login added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,18 +50,27 @@
              {{ csrf_field() }}
 
             <div class="form-group has-feedback">
-                <input type="email" class="form-control" placeholder="Email">
-                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                <input type="email" class="form-control" value="{{ old('email') }}" placeholder="Email"  autofocus>
+                @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                @endif
             </div>
-            <div class="form-group has-feedback">
-                <input type="password" class="form-control" placeholder="Password">
-                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            <div class="form-group has-feedback {{ $errors->has('password') ? ' has-error' : '' }}">
+                <input type="password" class="form-control" placeholder="Password" >
+
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
             </div>
             <div class="row">
                 <div class="col-xs-7">
                     <div class="checkbox">
                         <label>
-                            <input type="checkbox"> Remember Me
+                            <input type="checkbox"{{ old('remember') ? 'checked' : '' }}> Remember Me
                         </label>
                     </div>
                 </div>
@@ -83,7 +92,7 @@
         <!-- /.social-auth-links -->
 
         <a href="{{ route('password.request') }}">I forgot my password</a><br>
-        <a href="register.html" class="text-center">Register a new membership</a>
+        <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
 
     </div>
     <!-- /.login-box-body -->
